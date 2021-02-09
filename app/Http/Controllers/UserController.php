05e59e8e1e89e18bec3254364d31a6acd2b2d82a
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-02-09 09:19:29 +0700

    Lumen Auth App

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class Controller extends Controller
+{
+    /**
+     * Instantiate a new UserController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return Response
+     */
+    public function profile() {
+        return response()->json(['user' => Auth::user()], 200);
+    }
+
+    /**
+     * Get all User.
+     *
+     * @return Response
+     */
+    public function allUsers() {
+        return response()->json(['user' => User::all()], 200);
+    }
+
+    /**
+     * Get one user.
+     *
+     * @return Response
+     */
+    public function singelUser($id) {
+        try {
+            $user = User::findOrFail($id);
+
+            return response()->json(['user' => $user], 200);
+        } catch {
+            return response()->json(['message' => 'User not found!', 404]);
+        }
+    }
+}
