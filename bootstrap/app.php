0dd7a14809a95ce0acaa8c154a57ce5199986b56
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-02-09 08:12:06 +0700

    Lumen Auth App

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,9 +91,10 @@ $app->routeMiddleware([
 |
 */
 
-$app->register(App\Providers\AppServiceProvider::class);
-$app->register(App\Providers\AuthServiceProvider::class);
-$app->register(App\Providers\EventServiceProvider::class);
+// $app->register(App\Providers\AppServiceProvider::class);
+// $app->register(App\Providers\AuthServiceProvider::class);
+// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
