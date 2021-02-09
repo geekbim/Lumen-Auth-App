1fe555df5a9f9da5cbb69ec5ca7d6e369768fad7
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-02-09 09:08:34 +0700

    Lumen Auth App

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,13 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     // Matches "/api/login
     $router->post('login', 'AuthController@login');
 
+    // Matches "/api/profile
+    $router->get('profile', 'UserController@profile');
+
+    // Matches "/api/users/1
+    $router->get('users/{id}', 'UserController@singleUser');
+
+    // Matches "/api/users
+    $router->get('users', 'UserController@allUsers');
+
 });
